bc3817763eb9ac73e05fc7abe42fe27bd2ad1b3c
Jacob Berman <jacobberman1995@gmail.com>
2018-03-29 20:40:45 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "fizzBuzz"
+  end
+  elsif number % 3 == 0
+      "Fizz"
+    end
+  elsif number % 5 == 0
+    "Buzz"
+  end 
+  else 
+    NIL
+  end 
+end 
